3e106fffccc7341e178a1dd4fb41efc28c9680da
Jose Daniel Fuentes <jdanielfuentes82@gmail.com>
2026-02-01 15:01:17 +0100

    feat: componente Header con breadcrumbs y carrito

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import ProductListPage from "./pages/ProductListPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import "./App.css";
@@ -6,10 +7,13 @@ import "./App.css";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ProductListPage />} />
-        <Route path="/product/:id" element={<ProductDetailPage />} />
-      </Routes>
+      <Header />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<ProductListPage />} />
+          <Route path="/product/:id" element={<ProductDetailPage />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,53 @@
+import { Link, useLocation } from "react-router-dom";
+import { useCart } from "../hooks/useCart";
+import "./Header.css";
+
+const Header = () => {
+  const { cartCount } = useCart();
+  const location = useLocation();
+
+  // Function to generate breadcrumbs based on the current path
+  const getBreadcrumbs = () => {
+    const path = location.pathname;
+
+    if (path === "/") {
+      return "Home";
+    }
+
+    if (path.startsWith("/product/")) {
+      return (
+        <>
+          <Link to="/">Home</Link>
+          <span> &gt; </span>
+          <span>Product</span>
+        </>
+      );
+    }
+
+    return "Home";
+  };
+
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="header-left">
+          <Link to="/" className="header-logo">
+            Phone Shop
+          </Link>
+        </div>
+
+        <div className="header-center">
+          <nav className="breadcrumbs">{getBreadcrumbs()}</nav>
+        </div>
+
+        <div className="header-right">
+          <div className="cart-icon">
+            Cart <span className="cart-count">{cartCount}</span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
